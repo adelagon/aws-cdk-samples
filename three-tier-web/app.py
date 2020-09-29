fb4704635fead0ab5668f2bd4b6fdb6b738449d1
Alvin Delagon <adelagon@amazon.com>
2020-09-29 19:41:07 +0800

    Implement environment separation

--- three-tier-web/app.py
+++ three-tier-web/app.py
@@ -4,11 +4,16 @@ from aws_cdk import core
 
 from three_tier_web.three_tier_web_stack import ThreeTierWebStack
 
+# Demonstrates how to externalize configurations on your stack
 config = yaml.load(open('./config.yaml'), Loader=yaml.FullLoader)
 
 app = core.App()
+
+# Create a different stack depending on the environment
+# Useful if you are just deploying on a single AWS account
+# Otherwise use core.Environments
 stack_name = "three-tier-web-" + config["environment"]
 
-ThreeTierWebStack(app, "three-tier-web", config)
+ThreeTierWebStack(app, stack_name, config)
 
 app.synth()
